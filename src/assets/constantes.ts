199f708359ec9839d71976d24d6e7405bf8fa2b3
OverCode <avalosfigueroapedro@gmail.com>
2024-07-07 21:32:43 -0500

    feat: Agregar constantes de juego y combinaciones ganadoras

---
+++ src/assets/constantes.ts
@@ -0,0 +1,24 @@
+export const FILAS = {
+    primeraFila: [12, 8, 4, 0],
+    segundaFila: [13, 9, 5, 1],
+    terceraFila: [14, 10, 6, 2],
+    cuartaFila: [15, 11, 7, 3]
+}
+
+export const WINNER_COMBINATIONS = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    [0, 5, 10, 15],
+    [3, 6, 9, 12]
+]
+
+export const TURNOS = {
+    amarillo: 'amarillo',
+    rojo: 'rojo'
+}
