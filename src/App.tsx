1ebc5611b548934463d67c735efe4d861318c395
OverCode <avalosfigueroapedro@gmail.com>
2024-07-07 21:32:20 -0500

    feat: Implementar juego 4 en raya con tablero y fichas interactivas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,89 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Tablero } from './assets/components/Tablero'
+import { Square } from './assets/components/Square'
+import { TURNOS } from './assets/constantes'
+import { agregarFicha, verificarWinner } from './assets/logica/tablero'
+import { guardarPartida, borrarPartida } from './assets/logica/localStorage'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tablero, setTablero] = useState(() => {
+    const tableroGuardado = localStorage.getItem('tablero');
+
+    return tableroGuardado ? JSON.parse(tableroGuardado) : Array(16).fill(null);
+  });
+
+  const [turno, setTurno] = useState(() => {
+    const turnoGuardado = localStorage.getItem('turno');
+
+    return turnoGuardado ? turnoGuardado : TURNOS.amarillo;
+  });
+
+  const [winner, setWinner] = useState<string | null>(() => {
+    const winnerGuardado = localStorage.getItem('winner');
+
+    return winnerGuardado ? JSON.parse(winnerGuardado) : null;
+  });
+
+  const actualizarTablero = (index: number) => {
+    const nuevoTablero = [...tablero];
+
+    const newTurno = agregarFicha(index, nuevoTablero, turno, actualizarTurno);
+
+    const newWinner = verificarWinner(nuevoTablero);
+
+    if (!winner) {
+      setTablero(nuevoTablero);
+
+      if (newWinner) {
+        setWinner(newWinner);
+        setTurno(newWinner);
+      }
+    }
+
+    if (newTurno) {
+      guardarPartida({ tablero: nuevoTablero, newTurno, newWinner });
+    }
+  }
+
+  const ReiniciarJuego = () => {
+    setTablero(Array(16).fill(null));
+    setTurno(TURNOS.amarillo);
+    setWinner(null);
+    borrarPartida();
+  }
+
+  const actualizarTurno = () => {
+    const newTurno = turno === TURNOS.amarillo ? TURNOS.rojo : TURNOS.amarillo;
+    setTurno(newTurno);
+    return newTurno;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='tablero'>
+      <h1>4 en raya</h1>
+      <button className='buttonReset' onClick={ReiniciarJuego}>Reiniciar</button>
+      <Tablero tablero={tablero} actualizarTablero={actualizarTablero} />
+      <section className='turno'>
+        <Square turno={ winner === null ? turno : winner } />
+      </section>
+      {winner && (
+        <section className='winnerModal'>
+          <div className='winner'>
+            <h3>
+              {winner === 'empate' ? 'Empate' : 'Ganador'}
+            </h3>
+            {
+              winner !== 'empate' && <Square turno={winner} />
+            }
+            <button className='buttonReset' onClick={ReiniciarJuego}>Reiniciar</button>
+          </div>
+        </section>
+      )}
+    </main>
   )
 }
 
